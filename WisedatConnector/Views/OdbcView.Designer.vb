de154641456cb7b43dcbebd12f8022dd74d22e1b
Hélio F. Cabral <helfcabral@gmail.com>
2024-02-08 21:46:42 -0100

    ODBCView ComboboxEvents

--- WisedatConnector/Views/OdbcView.Designer.vb
+++ WisedatConnector/Views/OdbcView.Designer.vb
@@ -29,9 +29,9 @@ Partial Class OdbcView
         Label5 = New Label()
         Label6 = New Label()
         ComboBox1 = New ComboBox()
-        TextBox1 = New TextBox()
-        TextBox2 = New TextBox()
-        TextBox3 = New TextBox()
+        txtServer = New TextBox()
+        txtUser = New TextBox()
+        txtPassword = New TextBox()
         PictureBox1 = New PictureBox()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
@@ -40,18 +40,18 @@ Partial Class OdbcView
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label1.Location = New Point(14, 15)
+        Label1.Location = New Point(16, 20)
         Label1.Name = "Label1"
-        Label1.Size = New Size(244, 21)
+        Label1.Size = New Size(302, 28)
         Label1.TabIndex = 0
         Label1.Text = "Configuração de ligação ODBC"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(27, 49)
+        Label2.Location = New Point(31, 65)
         Label2.Name = "Label2"
-        Label2.Size = New Size(228, 15)
+        Label2.Size = New Size(286, 20)
         Label2.TabIndex = 1
         Label2.Text = "Configure os seus dados de acesso ODBC."
         ' 
@@ -59,9 +59,9 @@ Partial Class OdbcView
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
-        Label3.Location = New Point(246, 86)
+        Label3.Location = New Point(281, 115)
         Label3.Name = "Label3"
-        Label3.Size = New Size(54, 15)
+        Label3.Size = New Size(73, 20)
         Label3.TabIndex = 1
         Label3.Text = "Servidor:"
         ' 
@@ -69,9 +69,9 @@ Partial Class OdbcView
         ' 
         Label4.AutoSize = True
         Label4.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
-        Label4.Location = New Point(220, 116)
+        Label4.Location = New Point(251, 155)
         Label4.Name = "Label4"
-        Label4.Size = New Size(80, 15)
+        Label4.Size = New Size(102, 20)
         Label4.TabIndex = 1
         Label4.Text = "Autenticação:"
         ' 
@@ -79,9 +79,9 @@ Partial Class OdbcView
         ' 
         Label5.AutoSize = True
         Label5.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
-        Label5.Location = New Point(239, 145)
+        Label5.Location = New Point(273, 193)
         Label5.Name = "Label5"
-        Label5.Size = New Size(61, 15)
+        Label5.Size = New Size(81, 20)
         Label5.TabIndex = 1
         Label5.Text = "Utilizador:"
         ' 
@@ -89,9 +89,9 @@ Partial Class OdbcView
         ' 
         Label6.AutoSize = True
         Label6.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
-        Label6.Location = New Point(257, 174)
+        Label6.Location = New Point(294, 232)
         Label6.Name = "Label6"
-        Label6.Size = New Size(43, 15)
+        Label6.Size = New Size(55, 20)
         Label6.TabIndex = 1
         Label6.Text = "Senha:"
         ' 
@@ -100,54 +100,59 @@ Partial Class OdbcView
         ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
         ComboBox1.FormattingEnabled = True
         ComboBox1.Items.AddRange(New Object() {"Autenticação Windows", "Autenticação SQL Server"})
-        ComboBox1.Location = New Point(306, 113)
+        ComboBox1.Location = New Point(350, 151)
+        ComboBox1.Margin = New Padding(3, 4, 3, 4)
         ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(235, 23)
+        ComboBox1.Size = New Size(268, 28)
         ComboBox1.TabIndex = 2
         ' 
-        ' TextBox1
+        ' txtServer
         ' 
-        TextBox1.Location = New Point(306, 83)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(235, 23)
-        TextBox1.TabIndex = 1
+        txtServer.Location = New Point(350, 111)
+        txtServer.Margin = New Padding(3, 4, 3, 4)
+        txtServer.Name = "txtServer"
+        txtServer.Size = New Size(268, 27)
+        txtServer.TabIndex = 1
         ' 
-        ' TextBox2
+        ' txtUser
         ' 
-        TextBox2.Enabled = False
-        TextBox2.Location = New Point(306, 142)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(235, 23)
-        TextBox2.TabIndex = 3
+        txtUser.Enabled = False
+        txtUser.Location = New Point(350, 189)
+        txtUser.Margin = New Padding(3, 4, 3, 4)
+        txtUser.Name = "txtUser"
+        txtUser.Size = New Size(268, 27)
+        txtUser.TabIndex = 3
         ' 
-        ' TextBox3
+        ' txtPassword
         ' 
-        TextBox3.Enabled = False
-        TextBox3.Location = New Point(306, 171)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(235, 23)
-        TextBox3.TabIndex = 4
-        TextBox3.UseSystemPasswordChar = True
+        txtPassword.Enabled = False
+        txtPassword.Location = New Point(350, 228)
+        txtPassword.Margin = New Padding(3, 4, 3, 4)
+        txtPassword.Name = "txtPassword"
+        txtPassword.Size = New Size(268, 27)
+        txtPassword.TabIndex = 4
+        txtPassword.UseSystemPasswordChar = True
         ' 
         ' PictureBox1
         ' 
         PictureBox1.Image = My.Resources.Resources.ODBC
-        PictureBox1.Location = New Point(51, 83)
+        PictureBox1.Location = New Point(58, 111)
+        PictureBox1.Margin = New Padding(3, 4, 3, 4)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(132, 111)
+        PictureBox1.Size = New Size(151, 148)
         PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox1.TabIndex = 5
         PictureBox1.TabStop = False
         ' 
         ' OdbcView
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.White
         Controls.Add(PictureBox1)
-        Controls.Add(TextBox3)
-        Controls.Add(TextBox2)
-        Controls.Add(TextBox1)
+        Controls.Add(txtPassword)
+        Controls.Add(txtUser)
+        Controls.Add(txtServer)
         Controls.Add(ComboBox1)
         Controls.Add(Label6)
         Controls.Add(Label5)
@@ -155,8 +160,9 @@ Partial Class OdbcView
         Controls.Add(Label3)
         Controls.Add(Label2)
         Controls.Add(Label1)
+        Margin = New Padding(3, 4, 3, 4)
         Name = "OdbcView"
-        Size = New Size(572, 239)
+        Size = New Size(654, 319)
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
@@ -169,8 +175,8 @@ Partial Class OdbcView
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtServer As TextBox
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents txtPassword As TextBox
     Friend WithEvents PictureBox1 As PictureBox
 End Class
--- WisedatConnector/Views/OdbcView.vb
+++ WisedatConnector/Views/OdbcView.vb
@@ -9,4 +9,14 @@
 
     End Sub
 
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        If ComboBox1.SelectedIndex = 0 Then
+            txtUser.Enabled = False
+            txtPassword.Enabled = False
+        Else
+            txtUser.Enabled = True
+            txtPassword.Enabled = True
+        End If
+    End Sub
 End Class
