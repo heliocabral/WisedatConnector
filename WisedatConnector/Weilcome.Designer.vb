c8f2c6c9e70b5b6d4a53eecc2866c0a15dfe5186
Hélio F. Cabral <helfcabral@gmail.com>
2024-02-07 23:14:40 -0100

    Creation of the "Welcome" View

---
+++ WisedatConnector/Weilcome.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Weilcome
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(14, 15)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(382, 21)
+        Label1.TabIndex = 0
+        Label1.Text = "Bem-vindo ao assistente de exportação de dados"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(27, 49)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(358, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Este assistente vai ajudálo a exportar dados  da aplicação WISEDAT."
+        ' 
+        ' Weilcome
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Weilcome"
+        Size = New Size(416, 78)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ WisedatConnector/Weilcome.vb
@@ -0,0 +1,3 @@
+﻿Public Class Weilcome
+
+End Class
